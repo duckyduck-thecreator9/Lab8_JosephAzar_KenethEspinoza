c2c3067fb9dcbd0afa1ad8c130af81ca67257fa5
KennethEspinoza14 <116386154+KennethEspinoza14@users.noreply.github.com>
2022-12-02 15:58:41 -0600

    avance archivos

--- JavaApplication45/src/javaapplication45/admin_jugador.java
+++ JavaApplication45/src/javaapplication45/admin_jugador.java
@@ -1,14 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication45;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 /**
  *
  * @author Kenneth
  */
 public class admin_jugador {
+
+    ArrayList<Jugador> jugadores = new ArrayList();
+    private File archivo = null;
+
+    public admin_jugador() {
+    }
+
+    public admin_jugador(String path) {
+        archivo = new File(path);
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
     
+    public void setJugador(Jugador j){
+        jugadores.add(j);
+    }
+
+    @Override
+    public String toString() {
+        return "admin_jugador{" + "jugadores=" + jugadores + ", archivo=" + archivo + '}';
+    }
+
+    public void cargarArchivo() {
+
+        try {
+            jugadores = new ArrayList();
+            Jugador temp;
+            if (archivo.exists()) {
+                FileInputStream entrada
+                        = new FileInputStream(archivo);
+                ObjectInputStream objeto
+                        = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (Jugador) objeto.readObject()) != null) {
+                        jugadores.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del archivo
+                }
+                objeto.close();
+                entrada.close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+
+        try {
+            fw = new FileOutputStream(archivo);
+            bw = new ObjectOutputStream(fw);
+            for (Jugador t : jugadores) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
 }
